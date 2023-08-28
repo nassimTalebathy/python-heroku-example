7c1eb7c7978ea1f9524cac2224903a61c494d513
Ricky Smith <rickysaulsmith@yahoo.com>
2023-08-28 12:49:12 +0200

    Update input/output types

--- src/data_types.py
+++ src/data_types.py
@@ -7,26 +7,27 @@ try:
 except:
     import config
 
+
 class DataInput(BaseModel):
     array: List[List[float]]
+    verbose: bool = False
 
     class Config:
-        arbitrary_types_allowed=True
+        arbitrary_types_allowed = True
 
-    @validator('array', pre=True, allow_reuse=True)
+    @validator("array", pre=True, allow_reuse=True)
     def validate_array(array: List[float] | List[List[float]]):
-        assert len(array) > 0, 'Array must have at least one element'
+        assert len(array) > 0, "Array must have at least one element"
         if isinstance(array[0], float):
             array = [array]
         for el in array:
-            assert len(el) == config.NUM_COLS, f'Must have {config.NUM_COLS} columns'
+            assert len(el) == config.NUM_COLS, f"Must have {config.NUM_COLS} columns"
         return array
 
     def to_np_array(self) -> np.ndarray:
         return np.vstack(self.array, dtype=np.float64)
-    
-    
-    
+
+
 class DataOutput(BaseModel):
     predictions: List[float] = Field(..., min_items=1)
     start_time: float
--- src/main.py
+++ src/main.py
@@ -7,33 +7,39 @@ from src.log import logger
 
 
 # Global variable to store model after loading it
-model = None 
+model = None
 
 # FastAPI app
 app = FastAPI()
+
 
 @app.on_event("startup")
 def load_model():
     global model
     model = joblib.load(config.MODEL_PATH)
-    logger.info('Model loaded')
+    logger.info("Model loaded!")
     return
 
 
 @app.get("/health")
 def predict() -> dict:
-    return {'status':'OK', 'status_code': 200, 'model_version': config.MODEL_VERSION}
+    return {"status": "OK", "status_code": 200, "model_version": config.MODEL_VERSION}
 
 
 @app.post("/predict", response_model=DataOutput)
 def predict(input_data: DataInput) -> dict:
-    logger.info(f'Incoming request with data type {type(input_data)}')
-    logger.info(f'input_data:\n{input_data}')
+    if input_data.verbose:
+        logger.info(f"Incoming request with data type {type(input_data)}")
+        logger.info(f"input_data:\n{input_data[:2]}")
     start_time = time.time()
     np_array = input_data.to_np_array()
     predictions = model.predict_proba(np_array)[:, 1]
+    end_time = time.time()
+    if input_data.verbose:
+        logger.info(f"Predictions took {end_time - start_time:.1f} seconds")
+        logger.info(f"{predictions}")
     return {
         "predictions": predictions.tolist(),
         "start_time": start_time,
-        "time_taken": time.time() - start_time,
-    } 
+        "time_taken": end_time - start_time,
+    }
